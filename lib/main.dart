d07a33fce3a4accde30b326de2256b34cb2e1973
eroshunor <51173183+eroshunor@users.noreply.github.com>
2022-03-11 14:00:03 +0200

    provider package added
    provider package added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:brew_crew/model/my_user.dart';
 import 'package:brew_crew/screens/wrapper.dart';
+import 'package:brew_crew/services/authentication_service%20.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,8 +17,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<MyUser?>.value(
+      value: AuthenticationService().user,
+      initialData: null,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
